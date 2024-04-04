bf95b3905cc3cc88a00548b75e378e18bc7db2c3
Prasanth Reddy Kasu <146632797+Prasanth-Reddy-Kasu@users.noreply.github.com>
2024-04-04 22:24:17 +0530

    Update api.py

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import sqlite3
 import requests
 from datetime import datetime, timedelta
 import json
+import os
 
 app = Flask(__name__)
 
@@ -132,4 +133,5 @@ def find_events():
     return json.dumps(pages_list, sort_keys=False)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Run the app using Gunicorn
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
